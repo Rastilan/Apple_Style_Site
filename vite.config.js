bd4cafc75be716268bf4a12adc603b7e94d4a697
Rastilan <christoperbhall@gmail.com>
2024-09-14 08:13:02 -0400

    type: feature. Finished adding 3d model of iphone and begun on features section

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: "chris-hall",
+    project: "javascript-react",
+  }), 
+],
+
+  build: {
+    sourcemap: true
+  }
 })
